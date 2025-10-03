2d4c87cbe86081440ce375e6de8312e00c503188
Charles Opitz <cjxopitz@gmail.com>
2025-10-03 00:22:17 -0500

    Create API request

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -9,13 +9,22 @@ import Foundation
 
 //PRIMARY API SERVICE OBJECT TO GET RICK AND MORTY DATA
 final class RMService {
+    
     //Shared Singleton you can access from anywhere in the entire app
     static let shared = RMService()
+    
     //Privatized constructor
     private init() {}
     
     //Send Rick and Morty API Call
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    //-Parameters:
+        //-request: Request instance
+        //-type: The type of object we expect to get back
+        //-completion: Callback with data or error
+    public func execute<T: Codable>(
+            _ request: RMRequest,
+            expecting type: T.Type,
+            completion: @escaping (Result<T, Error>) -> Void) {
         
     }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -13,7 +13,23 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
-        // Do any additional setup after loading the view.
+        
+        let request = RMRequest(
+            endpoint: .character,
+            //pathComponents: ["1"]
+            queryParameters: [
+            URLQueryItem(name: "name", value: "rick"),
+            URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        
+        print(request.url)
+        
+        RMService.shared.execute(request,
+                                 expecting: RMCharacter.self) { result in
+
+        }
+        
     }
     
 
