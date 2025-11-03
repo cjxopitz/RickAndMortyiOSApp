87850fbf3b1669162246b97ec2efa4eca1be1c47
Charles Opitz <cjxopitz@gmail.com>
2025-11-03 00:06:20 -0600

    Up to 4:55:10  Part 16 begins

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class RMCharacterDetailViewViewModel {
     
@@ -32,26 +33,76 @@ final class RMCharacterDetailViewViewModel {
     public var title: String {
         character.name.uppercased()
     }
-   
-    /// We're actually already grabbing the character, apparently?  Explanation at 4:22:00-4:23:00
-//    public func fetchCharacterData() {
-//        print(character.url)
-//        guard let url = requestUrl,
-//              let request = RMRequest(url: url) else {
-//            print("Failed to create")
-//            return
-//        }
-//        //print(request.url)
-//        RMService.shared.execute(request,
-//                                 expecting: RMCharacter.self) { result in
-//            switch result {
-//            case .success(let success):
-//                print(String(describing: success))
-//            case .failure(let failure):
-//                print(String(describing: failure))
-//            }
-//        }
-//        
-//    }
+    
+    //MARK: - Layout
+    public func createPhotoSectionLayout() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                     leading: 0,
+                                                     bottom: 10,
+                                                     trailing: 0)
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)
+            ),
+            subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    public func createInfoSectionLayout() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.5),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 2,
+            leading: 2,
+            bottom: 2,
+            trailing: 2
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item, item])
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    public func createEpisodeSectionLayout() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 5,
+            bottom: 10,
+            trailing: 8
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.8),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        return section
+    }
+    
     
 }
