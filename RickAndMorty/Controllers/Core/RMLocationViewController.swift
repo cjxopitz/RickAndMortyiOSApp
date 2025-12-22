b211f0689b7c70092a37fa9816bcac08f9a76d53
Charles Opitz <cjxopitz@gmail.com>
2025-12-21 22:24:11 -0600

    Up to 9:57:00  Part 36 begins

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 final class RMLocationViewController: UIViewController {
+    
+    private let primaryView = RMLocationView()
+    
+    private let viewModel = RMLocationViewViewModel()
+    
+    //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(primaryView)
         view.backgroundColor = .systemBackground
         title = "Locations"
         addSearchButton()
+        addConstraints()
 
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
     private func addSearchButton() {
